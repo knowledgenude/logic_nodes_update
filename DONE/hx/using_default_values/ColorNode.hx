666633b842b9e6807a3e7d24e6641fec784e5e1c
Henrique <phenriquefel@gmail.com>
2021-08-13 14:24:29 -0300

    Add RGB inputs to Color node

--- DONE/hx/using_default_values/ColorNode.hx
+++ DONE/hx/using_default_values/ColorNode.hx
@@ -6,19 +6,27 @@ class ColorNode extends LogicNode {
 
 	var value = new Vec4();
 
-	public function new(tree: LogicTree, r = 0.8, g = 0.8, b = 0.8, a = 1.0) {
+	public function new(tree: LogicTree, x: Null<Float> = null, y: Null<Float> = null, z: Null<Float> = null) {
 		super(tree);
 
-		value.set(r, g, b, a);
+		if (x != null) {
+			LogicNode.addLink(new FloatNode(tree, x), this, 0, 0);
+			LogicNode.addLink(new FloatNode(tree, y), this, 0, 1);
+			LogicNode.addLink(new FloatNode(tree, z), this, 0, 2);
+		}
 	}
 
 	override function get(from: Int): Dynamic {
-		if (inputs.length > 0) return inputs[0].get();
+		value = new Vec4();
+		value.x = inputs[0].get();
+		value.y = inputs[1].get();
+		value.z = inputs[2].get();
 		return value;
 	}
 
 	override function set(value: Dynamic) {
-		if (inputs.length > 0) inputs[0].set(value);
-		else this.value = value;
+		inputs[0].set(value.x);
+		inputs[1].set(value.y);
+		inputs[2].set(value.z);
 	}
 }
--- DONE/py/variable/LN_color.py
+++ DONE/py/variable/LN_color.py
@@ -1,14 +1,15 @@
 from arm.logicnode.arm_nodes import *
 
 class ColorNode(ArmLogicTreeNode):
-    """Stores the given color as a variable.
-    """
+    """Stores the given color as a variable."""
 
     bl_idname = 'LNColorNode'
     bl_label = 'Color'
     arm_version = 1
 
     def arm_init(self, context):
-        self.add_input('ArmColorSocket', 'Color In', default_value=[1.0, 1.0, 1.0, 1.0])
+        self.add_input('ArmFloatSocket', 'R', default_value=1.0)
+        self.add_input('ArmFloatSocket', 'G', default_value=1.0)
+        self.add_input('ArmFloatSocket', 'B', default_value=1.0)
 
-        self.add_output('ArmColorSocket', 'Color Out', is_var=True)
+        self.add_output('ArmColorSocket', 'Color', is_var=True)
